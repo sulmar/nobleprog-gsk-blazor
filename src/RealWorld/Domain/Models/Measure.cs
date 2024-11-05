a14716ed80e326552cf958bd37cf6d80295bb880
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-05 15:17:14 +0100

    Przekazywanie złożonego obiektu jako parametr kaskadowy

---
+++ src/RealWorld/Domain/Models/Measure.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Models;
+
+public class Measure
+{
+    public string Title { get; set; }
+    public int Value { get; set; }
+    public string Unit { get; set; }    
+
+    public Measure(string title, int value, string unit)
+    {
+        Title = title;
+        Value = value;
+        Unit = unit;
+    }
+}
