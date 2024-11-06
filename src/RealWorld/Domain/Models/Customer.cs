a4d762c054c8316576fa290870bb2d8066db49dc
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-06 11:14:54 +0100

    Dodanie walidacji za pomocą DataAnnotations

--- src/RealWorld/Domain/Models/Customer.cs
+++ src/RealWorld/Domain/Models/Customer.cs
@@ -1,7 +1,20 @@
-﻿namespace Domain.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
 
 public partial class Customer : BaseEntity
 {
+    [Required, MinLength(3)]
     public string Name { get; set; }
+
+    [MaxLength(10)]
     public string Description { get; set; }
+
+    [RegularExpression(@"\d*", ErrorMessage = "Only digits")]
+    public string Account { get; set; }
+
+    public string Password { get; set; }
+
+    [Compare(nameof(Password))]
+    public string ConfirmPassword { get; set; }
 }
