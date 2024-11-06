f0cf136ab9e04eac702e46051331fe6abddf0ee4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-11-06 11:28:20 +0100

    Dodanie walidacji za pomocą FluentValidation

---
+++ src/RealWorld/Domain/Validators/CustomerValidator.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models;
+using FluentValidation;
+
+namespace Domain.Validators;
+
+public class CustomerValidator : AbstractValidator<Customer>
+{
+    public CustomerValidator()
+    {
+        RuleFor(p => p.Name).NotEmpty().MinimumLength(3);
+        RuleFor(p => p.Description).MaximumLength(10);
+        RuleFor(p => p.Account).Matches(@"\d*").WithMessage("Only digits");
+        RuleFor(p => p.Password).Equal(p => p.ConfirmPassword);
+    }
+
+}
